aed9b0d8a03fcfaa35e9784682fa52c729495ea2
AjiMIde <chenjitao@phone580.com>
2019-08-29 10:47:52 +0800

    update button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bee-flex-box">
+      <div class="bee-flex-f-1">AAA</div>
+      <div class="bee-flex-f-2">BBB</div>
+      <div class="bee-flex-f-3">CCC</div>
+    </div>
+
+    <div class="btn-example">
+      <button class="bee-btn bee-btn-md">普通按钮</button>
+      <button class="bee-btn bee-btn-sm">小按钮</button>
+      <button class="bee-btn bee-btn-bg">大按钮</button>
+    </div>
+    <div class="btn-example">
+      <button class="bee-btn bee-btn-primary">首选项</button>
+      <button class="bee-btn bee-btn-danger">危险</button>
+      <button class="bee-btn bee-btn-success">成功</button>
+      <button class="bee-btn bee-btn-info">信息</button>
+      <button class="bee-btn bee-btn-warn">警告</button>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
 
 <style lang="scss">
-  @import "../styles/bee.m.reset";
-  @import "../styles/bee.m.markdown";
+  @import "../styles/bee.reset";
+  @import "../styles/bee.markdown";
+  @import "../styles/bee.flex";
+  @import "../styles/bee.btn";
+</style>
+<style lang="scss">
+  .home {
+    .bee-flex-box {
+      > div {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 4px 8px;
+      }
+    }
+  }
 </style>
