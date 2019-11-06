c1e4222bbb36d9fff913ae2c0873d2fa238580ba
AjiMIde <chenjitao@phone580.com>
2019-11-06 10:25:52 +0800

    Add toast and icon components

---
+++ src/components/bee-icon.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :style="blkStyle">
+    <svg class="bee-icon" aria-hidden="true" :color="color">
+      <use :xlink:href="'#icon-' + icon"></use>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    color: {
+      type: String,
+      default: '#333333'
+    },
+    fontSize: { // 指定宽度大小
+      type: Number,
+      default: 16
+    }
+  },
+  components: {
+  },
+  computed: {
+    blkStyle () {
+      return {
+        display: 'inline-block',
+        fontSize: this.fontSize + 'px'
+      }
+    }
+  },
+  data () {
+    return { }
+  },
+  methods () {
+  },
+  mounted () {
+  },
+  created () {
+  },
+  watch: {
+  }
+}
+</script>
+
+<style lang="scss">
+  // icon 预设
+  .bee-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
+
---
+++ src/components/dialog/bee-toast.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="bee-toast" v-if="show">
+    <div class="bee-t-mask bee-fixed"></div>
+    <div class="bee-t-container bee-fixed bee-fb bee-fb-C">
+      <div class="bee-t-body">
+        <div class="bee-t-loading-icon">
+          <bee-icon v-if="icon === 1" icon="success" color="#ffffff" :font-size="50"></bee-icon>
+          <bee-icon v-if="icon === 2" icon="fail" color="#ffffff" :font-size="50"></bee-icon>
+          <bee-icon v-if="icon === 3" icon="warn" color="#ffffff" :font-size="50"></bee-icon>
+        </div>
+        <div class="bee-t-loading-content">{{content || ''}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BeeIcon from '../bee-icon'
+
+export default {
+  components: { BeeIcon },
+  props: {
+  },
+  data () {
+    this.timeout = null
+
+    return {
+      show: false,
+
+      content: '',
+      icon: 0, // 0 不设置icon, 1 设置为成功, 2 设置为不成功, 3 设置为 warning
+      duration: 1500,
+    }
+  },
+  methods: {
+    showToast (content = '', icon = 0, duration) {
+      this.content = content || ''
+      this.icon = [0, 1, 2, 3].indexOf(icon) === -1 ? 0 : icon
+      this.duration = duration || this.duration
+
+      window.clearTimeout(this.timeout)
+
+      this.show = true
+
+      if (parseInt(this.duration) === -1) return
+      this.timeout = window.setTimeout(() => {
+        this.hideToast()
+      }, parseInt(this.duration))
+    },
+    hideToast () {
+      this.show = false
+    },
+    toggleToast () {
+      this.show = !this.show
+    }
+  },
+  mounted () {
+    console.log(',,,,', this)
+  },
+  created () {
+    // this.$on('xxx', (a, b, c) => {
+    //   console.log(a, b, c)
+    // })
+  },
+  watch: {}
+}
+</script>
+
+<style lang="scss">
+  @import "../../styles/base/variable/color";
+  .bee-toast {
+    .bee-t-mask {
+
+    }
+    .bee-t-container {
+      justify-content: center;
+      align-items: center;
+    }
+    .bee-t-body {
+      background-color: $cl-bg-toast;
+      border-radius: 4px;
+      width: 50%;
+    }
+    .bee-t-loading-icon {
+      .bee-icon {
+        padding: 12px 0 6px 0;
+      }
+    }
+    .bee-t-loading-content {
+      color: #fff;
+      font-size: 1.2em;
+      padding: 12px 0;
+    }
+  }
+</style>
+
